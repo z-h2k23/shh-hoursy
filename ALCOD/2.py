11f18c530b8a56a97022031f347dd497fd9d9d38
Viswanath-Rao <146200848+Viswanath-Rao@users.noreply.github.com>
2023-10-15 16:55:23 +0530

    Commit

---
+++ ALCOD/2.py
@@ -0,0 +1,16 @@
+word="level"
+if word == word[::-1]:
+    print("palindrome")
+else:
+    print("not a palindrome")
+
+    
+palindrome
+word="malayalam"
+if word == word[::-1]:
+    print("palindrome")
+else:
+    print("not a palindrome")
+
+    
+palindrome
