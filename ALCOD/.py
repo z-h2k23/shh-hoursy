ea9b585347e46d1b161d526aaec7365057dfefa5
Viswanath-Rao <146200848+Viswanath-Rao@users.noreply.github.com>
2023-10-15 16:42:04 +0530

    Commit

---
+++ ALCOD/.py
@@ -0,0 +1,23 @@
+x=20
+for i in range(20,50)
+SyntaxError: incomplete input
+for i in range(20,50):
+    if i%2!=0:
+        print(i)
+
+        
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+49
