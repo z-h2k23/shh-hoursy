f1836badb782b6f6088b568b004ab9839df5da1c
Viswanath-Rao <146200848+Viswanath-Rao@users.noreply.github.com>
2023-10-15 16:49:51 +0530

    Commit

---
+++ ALCOD/1.py
@@ -0,0 +1,14 @@
+count=0
+while count<9:
+     print(count,"less than 5")
+     count=count+1 
+     
+0 less than 5
+1 less than 5
+2 less than 5
+3 less than 5
+4 less than 5
+5 less than 5
+6 less than 5
+7 less than 5
+8 less than 5
